d1e75d5bd1cc4ca9f18ead9f07bc2d86607bd253
klebergbatista <78322011+klebergbatista@users.noreply.github.com>
2021-11-07 17:35:46 -0300

    Add files via upload

---
+++ teste3.py
@@ -0,0 +1,11 @@
+n = (input('Digite um valor: '))
+print (n, 'detalhado:',"\n"
+       'Alpha Numérico:', n.isalnum(), "\n"
+       'Decimal:', n.isdecimal(), "\n"
+       'É do Alfabeto:', n.isalpha(), "\n" 
+       'É Espaço:', n.isspace(), "\n" 
+       'É maiuscula:', n.isupper(), "\n"
+       'É capitalizado:', n.istitle(), "\n"
+       'Type:',  (type (n)))
+
+
