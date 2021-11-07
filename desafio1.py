a6da891499a8338ae9cd73eefa318403be0068dc
klebergbatista <78322011+klebergbatista@users.noreply.github.com>
2021-11-07 17:28:43 -0300

    Add files via upload

---
+++ desafio1.py
@@ -0,0 +1,4 @@
+Dia = input ()
+Mês = input ()
+Ano = input ()
+print (' Sua data de nascimento é ', Dia, 'de', Mês, 'de', Ano)
---
+++ desafio3.py
@@ -0,0 +1,3 @@
+Primeiro = input ('Primeiro número ')
+Segundo = input ('Segundo número ')
+print ('A soma é', Primeiro+Segundo)
